b6c31316a3192eac8af6782c94790ac3bab44623
crownmueed1-oss <crownmueed1@gmail.com>
2026-03-29 00:20:33 +0500

    edit

---
+++ week 6/p6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float stu[5];
+stu[0]=1.66;
+stu[1]=4.56;
+stu[2]=5.55;
+stu[3]=12.99;
+stu[4]=44.09;
+cout<<"The second element of array = "<<stu[1]<<endl;
+cout<<"The  forth element of array = "<<stu[3]<<endl;
+}
