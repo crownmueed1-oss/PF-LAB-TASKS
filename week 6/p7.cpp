9ccb112e989ec37f0db919ba38148a2d489c8820
crownmueed1-oss <crownmueed1@gmail.com>
2026-03-29 00:23:51 +0500

    edit

---
+++ week 6/p7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int num[5];
+cout<<"Enter the first element= ";
+cin>>num[0];
+cout<<"Enter the second element= ";
+cin>>num[1];
+cout<<"Enter the third element= ";
+cin>>num[2];
+cout<<"Enter the forth element= ";
+cin>>num[3];
+cout<<"Enter the fifth element= ";
+cin>>num[4];
+cout<<"The first element of an array is "<<num[0]<<endl;
+cout<<"The last element  of an array is "<<num[4]<<endl;
+}
